442160865c89149a847d2ba0a3295f2664942c29
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-03-01 16:27:35 -0500

    Add apply operations to an array solution

---
+++ 2460. Apply Operations to an Array/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        vector<int> answers(nums.size(), 0);
+
+        for(int i = 0; i < nums.size()-1; i++){
+            if(nums[i] == nums[i+1]){
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+             }
+        }
+      
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++){
+              if(nums[i] != 0){
+                answers[j] = nums[i];
+                j++;
+            }
+        }
+        return answers;
+    }
+};
