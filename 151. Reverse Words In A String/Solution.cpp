cbb31be47a7ef2a6247278eb62b2605882316e2f
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-03-04 08:18:04 -0500

    Simplified reverse words in a string solution

--- 151. Reverse Words In A String/Solution.cpp
+++ 151. Reverse Words In A String/Solution.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     string reverseWords(string s) {
         int lastSpaceIndex = s.size();
-        int i = s.size() - 1;
+        int i = s.size()-1;
         string result = "";
-
         while(i >= 0){
-        
-            while(i >= 0 && s[i] == ' '){
+            while(i >= 0 && s[i] == ' '){ //Ignore spaces
                 i--;
             }
-
-            lastSpaceIndex = i + 1;
             if(i < 0) break;
-            
+
+            lastSpaceIndex = i;
+
             while(i >= 0 && s[i] != ' '){
                 i--;
             }
 
             if(result.empty()){
-                result = s.substr(i + 1, lastSpaceIndex - (i+1));
+                result = s.substr(i+1, lastSpaceIndex - i);
             }
             else {
-                result += " " + s.substr(i + 1, lastSpaceIndex - (i+1));
+                 result += " " + s.substr(i+1, lastSpaceIndex - i);
             }
         }
-        return result;
+    return result;
     }
 };
