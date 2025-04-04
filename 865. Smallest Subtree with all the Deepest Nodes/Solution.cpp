7668d949ca36868661ff5eb3e51dc2d677190454
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-04-04 02:13:41 -0400

    Add 865. Smallest Subtree with all the Deepest Nodes DFS Solution

---
+++ 865. Smallest Subtree with all the Deepest Nodes/Solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    pair<TreeNode*, int> findLCA(TreeNode*& node){
+        if(node == nullptr) return std::make_pair(nullptr, 0);
+        
+        auto [leftLCA, leftHeight] = findLCA(node->left);
+        auto [rightLCA, rightHeight] = findLCA(node->right);
+        
+        if(leftHeight == rightHeight) return std::make_pair(node, leftHeight + 1);
+        else if(leftHeight > rightHeight) return std::make_pair(leftLCA, leftHeight + 1);
+        else return std::make_pair (rightLCA, rightHeight + 1);
+    };
+
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode*& root) {
+        
+        auto [lca, height] = findLCA(root);
+        return lca;
+    }
+};
