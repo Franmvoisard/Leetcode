41df34aad75485cb0563360aa8cee7942e8fb442
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-04-16 22:32:05 -0400

    Add 2176. Count Equal and Divisible Pairs in an Array Brute force solution

---
+++ 2176. Count Equal and Divisible Pairs in an Array/BruteForceSolution.cpp
@@ -0,0 +1,15 @@
+// Time: O (n^2)
+// Space: O(1)
+
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int result = 0;
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if (nums[i] == nums[j] && i*j %k == 0) result++;
+            }
+        }
+        return result;
+    }
+};
