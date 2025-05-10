e896149a7b3118b22eee9f142f1170d8f4d574ba
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-05-10 19:54:54 -0400

    Updated 3. Longest Substring Without Repeating Characters with map

--- 3. Longest Substring Without Repeating Characters/SolutionWithUnorderedMap.cpp
+++ 3. Longest Substring Without Repeating Characters/SolutionWithUnorderedMap.cpp
@@ -1,23 +1,22 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> map;
         int left = 0;
-        int result = 0;
+        int inputSize = s.size();
+        unordered_map<char, int> lastSeenIndex;
+        int longestSubstring = 0;
 
-        for(int right = 0; right < s.size(); right++){
-            char currentCharacter = s[right];
-            auto it = map.find(currentCharacter);
-
-            if(it != map.end() && it->second >= left){ 
-                left = it->second + 1;
-                map[currentCharacter] = right;
+        for(int right = 0; right < inputSize; right++){
+            char currentChar = s[right];
+            
+            if(lastSeenIndex.count(currentChar) && left <= lastSeenIndex[currentChar]){
+                left = lastSeenIndex[currentChar] + 1;                
             }
-            else {
-                map[currentCharacter] = right;            
-            }
-            result = max(result, right - left + 1);
+            lastSeenIndex[currentChar] = right;
+            longestSubstring = max(longestSubstring, (right - left) + 1);
         }
-        return result;
+        return longestSubstring;
     }
 };
