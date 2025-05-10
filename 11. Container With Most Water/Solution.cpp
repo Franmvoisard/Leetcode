715db8f6d7a49cb861c0aa68fabf56fef666d774
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-05-10 03:49:10 -0400

    Update 11. Container With Most Water Solution

--- 11. Container With Most Water/Solution.cpp
+++ 11. Container With Most Water/Solution.cpp
@@ -1,23 +1,22 @@
+// Time complexity O(N)
+// Space complexity O(1)
 class Solution {
 public:
     int maxArea(vector<int>& height) {
         int left = 0;
-        int right = height.size()-1;
-        int maxWaterCapacity = 0;
+        int right = height.size() - 1;
+        int maxWater = 0;
 
         while(left < right){
-
-            int diff = right - left;
-            int lowerHeight = min(height[left], height[right]);
-            if(height[left] <=  height[right]){
+            int leftHeight = height[left];
+            int rightHeight = height[right];
+            int water = min(rightHeight, leftHeight) * (right - left);
+            maxWater = max(maxWater, water);
+            if(leftHeight < rightHeight){
                 left++;
             }
-            else {
-                right--;
-            }
-
-            maxWaterCapacity = max(maxWaterCapacity, lowerHeight * diff);
+            else right--;
         }
-        return maxWaterCapacity;
+        return maxWater;
     }
 };
