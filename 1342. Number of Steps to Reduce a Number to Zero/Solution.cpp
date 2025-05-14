ad1c765f924811341bad79569bbab79ebb5b47fd
Franco Voisard <36276842+Franmvoisard@users.noreply.github.com>
2025-05-14 17:43:44 -0400

    Add 1342. Number of Steps to Reduce a Number to Zero Solution

---
+++ 1342. Number of Steps to Reduce a Number to Zero/Solution.cpp
@@ -0,0 +1,19 @@
+// Time Complexity: O(logn)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps = 0;
+        while (num > 0){
+            if(num % 2 == 0){
+                num /= 2;
+            }
+            else {
+                num--;
+            }
+            steps++;
+        }
+        return steps;
+    }
+};
